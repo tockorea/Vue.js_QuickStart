cc704ab43f55aa8b2ec735120cc23654a1687d12
tockorea <lim.chae.eun@gmail.com>
2018-03-25 23:34:28 +0900

    CHAPTER 12 vue-router를 이용한 라우팅

---
+++ ch12/contactsapp01/src/store/state.js
@@ -0,0 +1,12 @@
+import Constant from '../constant';
+import CONF from '../Config';
+
+export default {
+  contact: { no: 0, name: '', tel: '', address: '', photo: '' },
+  contactlist: {
+    pageno: 1,
+    pagesize: CONF.PAGESIZE,
+    totalcount: 0,
+    contacts: []
+  }
+}
