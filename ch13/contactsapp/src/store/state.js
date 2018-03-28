1ea3f2a94e17716f9b49749a1e8fe1a3892aa4a9
tockorea <lim.chae.eun@gmail.com>
2018-03-28 23:21:08 +0900

    CHAPTER 13 트랜지션 효과

---
+++ ch13/contactsapp/src/store/state.js
@@ -0,0 +1,13 @@
+import Constant from '../constant';
+import CONF from '../Config';
+
+export default {
+  isloading: false,
+  contact: { no: 0, name: '', tel: '', address: '', photo: '' },
+  contactlist: {
+    pageno: 1,
+    pagesize: CONF.PAGESIZE,
+    totalcount: 0,
+    contacts: []
+  }
+}
---
+++ ch13/todolistapp/src/store/state.js
@@ -0,0 +1,8 @@
+export default {
+  todolist: [
+    { todo: "영화보기", done: false },
+    { todo: "주말 산택", done: true },
+    { todo: "ES6 학습", done: false },
+    { todo: "잠실 야구장", done: false }
+  ]
+}
